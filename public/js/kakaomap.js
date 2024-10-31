e713a919aecf3b24c647c9e74e192d502794c20b
overjjang <overjjang99@gmail.com>
2024-10-31 14:26:14 +0900

    마커 추가

--- public/js/kakaomap.js
+++ public/js/kakaomap.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var container = document.getElementById('map');
     if (container) {
-        var options = {
-            center: new kakao.maps.LatLng(36.512884, 127.245108),
-            level: 3
-        };
+        var markerPosition  = new kakao.maps.LatLng(36.512884, 127.245108);
+
+        var options = {center: markerPosition, level: 3};
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({ position: markerPosition });
 
         var map = new kakao.maps.Map(container, options);
-    } else {
+        marker.setMap(map);
+    }
+    else {
         console.error("Map container element not found");
     }
 });
